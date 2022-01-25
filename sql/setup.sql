3acb1485372a501e0173272ec0190053e1ffd353
Zack Lyon <zack.j.lyon@gmail.com>
2022-01-25 15:23:04 -0800

    Add database and post route with model and controller

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,12 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS resources;
+
+CREATE TABLE resources (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  category TEXT NOT NULL,
+  title TEXT NOT NULL,
+  description TEXT,
+  url TEXT NOT NULL
+)
