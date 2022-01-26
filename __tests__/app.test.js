c8ddb0b1d3ccdd668944f7f24398112da935f100
Zack Lyon <87877729+ZackLyon@users.noreply.github.com>
2022-01-25 17:50:07 -0800

    Update app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const Resource = require('../lib/models/resource.js');
+const Resource = require('../lib/models/Resource.js');
 
 describe('backend routes', () => {
   beforeEach(() => {
